f3d4201114b0b5edde94b938bcc08d031db216f6
Abbas Rabbani <mail.abbas.rabbani@gmail.com>
2024-01-14 01:32:37 +0500

    Setup repo

---
+++ posts/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path("", views.index, name="homepage"),
+    path("post/<int:id", views.post_detail,name='post_detail')
+]
